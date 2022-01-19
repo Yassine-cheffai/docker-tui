888d02ec3e91620933c151567d9a43064ac7e3fb
Yassine-cheffai <yacincheffai@yahoo.com>
2022-01-19 21:59:05 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello docker tui!");
+use bollard::Docker;
+use bollard::image::ListImagesOptions;
+use std::default::Default;
+
+#[tokio::main]
+async fn main() {
+    let docker = Docker::connect_with_socket_defaults().unwrap();
+    let images = &docker.list_images(Some(ListImagesOptions::<String> {
+            all: true,
+            ..Default::default()
+        })).await.unwrap();
+
+        for image in images {
+            println!("-> {:?}", image);
+        }
 }
